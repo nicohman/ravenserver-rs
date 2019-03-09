ee5a0c039acb441089b20762fc15ea200d67451b
nicohman <nicohman@demenses.net>
2019-03-09 13:12:56 -0800

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
+extern crate rocket;
+extern crate mongodb;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate bson;
+mod themes;
+fn main() {
+    println!("Hello, world!");
+}
