b5d2dd6b02efa88c5d00cc583630f86668f1a084
nicohman <nicohman@demenses.net>
2019-03-11 14:36:24 -0700

    Add author view

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,7 @@ extern crate rocket_contrib;
 extern crate serde;
 #[macro_use]
 extern crate lazy_static;
-#[macro_use]
 extern crate serde_derive;
-use databases::mongodb::db::ThreadedDatabase;
 use mongodb::coll::options::*;
 use mongodb::{to_bson, Bson};
 use ravenserver::mongo::*;
@@ -21,7 +19,6 @@ use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 #[database("mongodb")]
 struct DbConnection(databases::mongodb::db::Database);
-use mongodb::pool::*;
 lazy_static! {
     static ref CONFIG: HashMap<String, Bson> = {
         let mut map = HashMap::new();
@@ -45,36 +42,66 @@ fn render_themes(themes: Vec<Theme>, mut context: HashMap<String, Bson>) -> Temp
     context.insert("themes".to_string(), to_bson(&themes).unwrap());
     Template::render("themes", &context)
 }
-#[get("/")]
-fn index(conn: DbConnection) -> Template {
+fn render_themes_view(
+    conn: DbConnection,
+    filter: Option<mongodb::Document>,
+    options: Option<FindOptions>,
+    ptitle: impl Into<String>,
+    constraints: Option<impl Into<String>>,
+) -> Template {
+    let (ptitle, constraints) = (
+        ptitle.into(),
+        constraints.map_or(String::default(), |x| x.into()),
+    );
     let db = DataBase::from_db(conn.0.clone()).unwrap();
     let themes = db
-        .find::<Theme>(doc!(), None)
+        .find::<Theme>(filter.unwrap_or(doc!()), options)
         .unwrap()
         .into_iter()
         .filter_map(|x| x.ok())
         .collect::<Vec<Theme>>();
     let mut context = CONFIG.clone();
-    context.insert("ptitle".to_string(), bson!("All themes"));
+    context.insert("ptitle".to_string(), to_bson(&ptitle).unwrap());
+    context.insert("constraints".to_string(), to_bson(&constraints).unwrap());
     render_themes(themes, context)
+}
+#[get("/")]
+fn index(conn: DbConnection) -> Template {
+    let mut find = FindOptions::new();
+    find.sort = Some(doc! {
+        "installs":-1
+    });
+    render_themes_view(conn, None, Some(find), "All themes", None as Option<String>)
 }
 #[get("/recent")]
 fn recent(conn: DbConnection) -> Template {
     let mut find = FindOptions::new();
-    find.sort = Some(doc!{
+    find.sort = Some(doc! {
         "updated":  -1
     });
-    let db = DataBase::from_db(conn.0.clone()).unwrap();
-    let themes = db
-        .find::<Theme>(doc!(), Some(find))
-        .unwrap()
-        .into_iter()
-        .filter_map(|x| x.ok())
-        .collect::<Vec<Theme>>();
-    let mut context = CONFIG.clone();
-    context.insert("ptitle".to_string(), bson!("All themes"));
-    context.insert("constraints".to_string(), bson!("Sorted by most recent"));
-    render_themes(themes, context)
+    render_themes_view(
+        conn,
+        None,
+        Some(find),
+        "All themes",
+        Some("Sorted by most recent"),
+    )
+}
+#[get("/themes/users/view/<id>")]
+fn user_themes(conn: DbConnection, id: String) -> Template {
+    let mut find = FindOptions::new();
+    find.sort = Some(doc! {
+        "installs":-1
+    });
+    render_themes_view(
+        conn,
+        Some(doc! {
+            "author":id.as_str()
+        }),
+        Some(find),
+        format!("All themes by {}", id),
+        None as Option<String>,
+    )
 }
 fn main() {
     rocket::ignite()
